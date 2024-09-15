35760a05acafa6867fee87f562f32f57bcc40d78
Matthew Quijano <mq022002@gmail.com>
2024-09-15 13:00:19 -0400

    feat: Simple page to show off fetched plants data

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./HomePage.module.css";
+import getPlants from "@/app/utils/getPlants";
+
+interface Plant {
+  id: number;
+  name: string;
+  type: string;
+  origin: string;
+}
 
 export default function HomePage() {
-  return <div className={styles.container}>Welcome to the Home Page!</div>;
+  const [plants, setPlants] = useState<Plant[]>([]);
+
+  useEffect(() => {
+    getPlants()
+      .then((data) => setPlants(data))
+      .catch((error) =>
+        console.error("An error occurred while fetching the plants:", error)
+      );
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Origin</th>
+          </tr>
+        </thead>
+        <tbody>
+          {plants.map((plant) => (
+            <tr key={plant.id}>
+              <td>{plant.id}</td>
+              <td>{plant.name}</td>
+              <td>{plant.type}</td>
+              <td>{plant.origin}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
