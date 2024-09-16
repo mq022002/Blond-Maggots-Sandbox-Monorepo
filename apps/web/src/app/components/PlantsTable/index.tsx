ebc6b9bd7a1d2d031b5f60a04e888fbaa0728ad5
Matthew Quijano <mq022002@gmail.com>
2024-09-16 17:03:01 -0400

    refactor: Move PlantsTable to separate component

---
+++ apps/web/src/app/components/PlantsTable/index.tsx
@@ -0,0 +1,47 @@
+interface Plant {
+  id: number;
+  name: string;
+  type: string;
+  origin: string;
+}
+
+interface PlantsTableProps {
+  plants: Plant[];
+}
+
+const PlantsTable: React.FC<PlantsTableProps> = ({ plants }) => (
+  <div className="flex justify-center items-center py-20">
+    <table className="w-4/5 border-collapse border border-black mx-auto overflow-auto">
+      <thead>
+        <tr>
+          <th className="p-4 text-left border border-black bg-green-500 text-white">
+            ID
+          </th>
+          <th className="p-4 text-left border border-black bg-green-500 text-white">
+            Name
+          </th>
+          <th className="p-4 text-left border border-black bg-green-500 text-white">
+            Type
+          </th>
+          <th className="p-4 text-left border border-black bg-green-500 text-white">
+            Origin
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {plants.map((plant) => (
+          <tr key={plant.id}>
+            <td className="p-4 text-left border border-black">{plant.id}</td>
+            <td className="p-4 text-left border border-black">{plant.name}</td>
+            <td className="p-4 text-left border border-black">{plant.type}</td>
+            <td className="p-4 text-left border border-black">
+              {plant.origin}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+export default PlantsTable;
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import getPlants from "@/app/utils/getPlants";
+import PlantsTable from "@/app/components/PlantsTable";
 
 interface Plant {
   id: number;
@@ -21,42 +22,5 @@ export default function HomePage() {
       );
   }, []);
 
-  return (
-    <div className="flex justify-center items-center py-20">
-      <table className="w-4/5 border-collapse border border-black mx-auto overflow-auto">
-        <thead>
-          <tr>
-            <th className="p-4 text-left border border-black bg-green-500 text-white">
-              ID
-            </th>
-            <th className="p-4 text-left border border-black bg-green-500 text-white">
-              Name
-            </th>
-            <th className="p-4 text-left border border-black bg-green-500 text-white">
-              Type
-            </th>
-            <th className="p-4 text-left border border-black bg-green-500 text-white">
-              Origin
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {plants.map((plant) => (
-            <tr key={plant.id}>
-              <td className="p-4 text-left border border-black">{plant.id}</td>
-              <td className="p-4 text-left border border-black">
-                {plant.name}
-              </td>
-              <td className="p-4 text-left border border-black">
-                {plant.type}
-              </td>
-              <td className="p-4 text-left border border-black">
-                {plant.origin}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  return <PlantsTable plants={plants} />;
 }
