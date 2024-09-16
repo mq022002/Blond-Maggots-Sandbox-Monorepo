3f4706911e79999c68a21c0ebe2b5eafd191c6f8
Matthew Quijano <mq022002@gmail.com>
2024-09-16 14:12:10 -0400

    refactor: Rename fetchPlants to getPlants

--- apps/web/src/app/utils/getPlants.ts
+++ apps/web/src/app/utils/getPlants.ts
@@ -1,4 +1,4 @@
-export default async function fetchPlants() {
+export default async function getPlants() {
   const response = await fetch("http://127.0.0.1:5000/api/plants");
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
